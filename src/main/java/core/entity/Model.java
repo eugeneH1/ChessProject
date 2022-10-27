3b158c9d1b76300289ca5da8a1f81fa102a5c17a
Eugene Heynike <eugeneheynike@icloud.com>
2022-10-27 20:45:09 +0200

    ShaderManager link debugged. still not sure what problem is but code is running

--- src/main/java/core/entity/Model.java
+++ src/main/java/core/entity/Model.java
@@ -4,10 +4,27 @@ public class Model {
 
     private int id;
     private int vertexCount;
+    private Texture texture;
 
     public Model(int id, int vertexCount) {
         this.id = id;
         this.vertexCount = vertexCount;
+    }
+
+    public Model(int id, int vertexCount, Texture texture) {
+        this.id = id;
+        this.vertexCount = vertexCount;
+        this.texture = texture;
+    }
+
+    public Model(Model model,Texture texture) {
+        this.id = model.getId();
+        this.vertexCount = model.getVertexCount();
+        this.texture = texture;
+    }
+
+    public Texture getTexture() {
+        return texture;
     }
 
     public int getId() {
